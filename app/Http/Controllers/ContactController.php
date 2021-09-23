e859de037718170395ed0e25e4067b320e5ea2c1
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-09-23 11:20:48 +0200

    push start

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function contact(){
+        return view('contact-us');
+    }
+
+    public function sendEmail(Request $request){
+        $details = [
+            'name' => $request->name,
+            'subject' => $request->subject,
+            'email' => $request->email,
+            'message' => $request->message,
+        ];
+
+        Mail::to('Charles.delalaing@gmail.com')->send(new ContactMail($details));
+
+        return back()->with('message_send', 'Your message has been sent successfully !');
+    }
+}
---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact-us</title>
+    <link rel="stylesheet" href={{ asset('css/app.css') }}>
+</head>
+
+<body>
+
+    <section>
+        <div class="container w-full mx-auto">
+            @if (Session::get('message_sent'))
+                <div class="alert alert-success" role="alert">
+                    {{ Session::get('message_sent') }}
+                </div>
+            @endif
+            <form method="post" action={{ route('contact.send') }} enctype="multipart/form-data">
+                @csrf
+                <!-- component -->
+                <div class="max-w-2xl bg-white py-10 px-5 m-auto w-full mt-10">
+                    <div class="text-3xl mb-6 text-center ">
+                        Contact us Now !
+                    </div>
+                    <div class="grid grid-cols-2 gap-4 max-w-xl m-auto">
+                        <div class="col-span-2 lg:col-span-3">
+                            <input type="text" name="name"
+                                class="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Name" />
+                        </div>
+                        <div class="col-span-2 lg:col-span-3">
+                            <input type="text" name="subject"
+                                class="border-solid border-gray-400 border-2 p-3 md:text-xl w-full" placeholder="Subject" />
+                        </div>
+                        <div class="col-span-2 lg:col-span-3">
+                            <input type="text" name="email" class="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                                value={{ Auth::user()->email}} />
+                        </div>
+                        <div class="col-span-2 lg:col-span-3">
+                            <textarea cols="30" name="message" rows="8"
+                                class="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                                placeholder="Message"></textarea>
+                        </div>
+                        <div class="col-span-3 text-right lg:col-span-3">
+                            <button type="submit" class="py-3 px-6 bg-green-500 text-white font-bold w-full sm:w-32">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+
+</body>
+
+</html>
