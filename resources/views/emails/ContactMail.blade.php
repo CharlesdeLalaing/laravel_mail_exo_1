4a00cd2fbab6e2d52b8f3a0f74bcec50355aac6e
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-09-23 15:45:01 +0200

    push

--- resources/views/emails/ContactMail.blade.php
+++ resources/views/emails/ContactMail.blade.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Form</title>
-</head>
-<body>
-    <h1>Contact Message</h1>
-    <p>Name : {{ $details['name'] }}</p>
-    <p>Email : {{ $details['email'] }}</p>
-    <p>Message : {{ $details['message'] }}</p>
-</body>
-</html>
+@extends('emails.base.base')
+@section('content')
+    <div class="bigDiv container bg-yellow-300">
+        <div class="bg-yellow-500 shadow-md borderb-2 text-center text-white font-italic text-lg">
+            <h1>Contact Message</h1>
+        </div>
+            <p>Name : {{ $details['name'] }}</p>
+            <p>Email : {{ $details['email'] }}</p>
+            <p>Message : {{ $details['message'] }}</p>
+    </div>
+@endsection
---
+++ resources/views/emails/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <section>
+        
+    </section>
+</header>
+
+
+
